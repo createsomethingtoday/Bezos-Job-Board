e6a7b0968ad5450c812b63ffca00556691a8e903
create.something <contact@micahryan.com>
2023-12-05 14:44:04 -0600

    Add job filtering and counts
    Fetch jobs from API and store in state
    Add Filter component to allow filtering jobs
    Store active filters in state
    Create filteredJobs state to hold filtered jobs
    Add useEffect to filter jobs when filters change
    Display count of total jobs and visible filtered jobs
    Pass filtered jobs to JobList component
    Allow adding and removing filters to update state

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,34 @@
-// pages/Home.js
-
 import { useEffect, useState } from 'react';
 import JobList from '../components/JobList';
 import Filter from '../components/Filter';
 import { fetchJobs } from '../services/greenhouseApi';
 
 export default function Home() {
-    const [jobs, setJobs] = useState([]);
-    const [filteredJobs, setFilteredJobs] = useState([]);
-    const [filters, setFilters] = useState([]);
+    const [jobs, setJobs] = useState([]); // All jobs
+    const [filteredJobs, setFilteredJobs] = useState([]); // Jobs after filtering
+    const [filters, setFilters] = useState([]); // Active filters
 
+    // Fetch jobs on component mount
     useEffect(() => {
         fetchJobs(true).then(data => {
             setJobs(data);
-            setFilteredJobs(data);
+            setFilteredJobs(data); // Initially, all jobs are visible
         });
     }, []);
 
+    // Handle addition of a new filter
     const handleFilterChange = (newFilter) => {
         if (!filters.includes(newFilter)) {
             setFilters([...filters, newFilter]);
         }
     };
 
+    // Handle removal of a filter
     const handleRemoveFilter = (filterToRemove) => {
         setFilters(filters.filter(filter => filter !== filterToRemove));
     };
 
+    // Apply filters to jobs
     useEffect(() => {
         const filtered = jobs.filter(job =>
             filters.every(filter =>
@@ -44,6 +46,7 @@ export default function Home() {
                 filters={filters}
                 onRemoveFilter={handleRemoveFilter}
             />
+            <p>{`${filteredJobs.length} of ${jobs.length}`}</p> {/* Display count of visible jobs */}
             <JobList jobs={filteredJobs} />
         </div>
     );
