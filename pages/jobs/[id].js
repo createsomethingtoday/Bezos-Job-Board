1338c59a6fd289d36fa1fb534ea24678cbcdcee0
create.something <contact@micahryan.com>
2023-12-05 14:19:04 -0600

    Add job details page and fetch job data from API
    Create JobPage component to display job details
    Fetch job data from Greenhouse API in greenhouseApi.js
    Add useEffect hook to get job data on page load
    Display loading state while fetching data
    Render JobDetails component with fetched data
    Handle and display errors

--- pages/jobs/[id].js
+++ pages/jobs/[id].js
@@ -1,12 +1,32 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import JobDetails from '../../components/JobDetails';
 import { fetchJobDetails } from '../../services/greenhouseApi';
 
 const JobPage = () => {
   const router = useRouter();
   const { id } = router.query;
+  const [jobDetails, setJobDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const jobDetails = fetchJobDetails(id); // Implement this in greenhouseApi.js
+  useEffect(() => {
+    if (id) {
+      fetchJobDetails(id)
+        .then(data => {
+          setJobDetails(data);
+          setLoading(false);
+        })
+        .catch(err => {
+          console.error(err);
+          setError('Failed to fetch job details');
+          setLoading(false);
+        });
+    }
+  }, [id]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return <JobDetails job={jobDetails} />;
 };
