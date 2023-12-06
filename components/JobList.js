714e70c137136e9fd7f00875a76e45f30ce7d6ed
create.something <contact@micahryan.com>
2023-12-05 19:56:06 -0600

    Implement JobItem component to display job data
    Adds a JobItem React component that takes in a job object prop and displays the job title, update date, location, departments, and a link to view the full job posting. It uses a JobList CSS module for styling. This component could be used to display job data in a list or grid.

--- components/JobList.js
+++ components/JobList.js
@@ -1,23 +1,13 @@
-// components/JobList.js
+import JobItem from './JobItem';
 
 function JobList({ jobs }) {
-    return (
-        <div>
-            <h2>Job Listings</h2>
-            <ul>
-                {jobs.map((job, index) => (
-                    <li key={index}>
-                        <h3>{job.title}</h3>
-                        <p><strong>Location:</strong> {job.location.name}</p>
-                        <p><strong>Updated At:</strong> {job.updated_at}</p>
-                        <p><strong>Department:</strong> {job.departments.map(dept => dept.name).join(', ')}</p>
-                        <p><strong>Office:</strong> {job.offices.map(office => office.name).join(', ')}</p>
-                        <a href={job.absolute_url} target="_blank" rel="noopener noreferrer">View Job Details</a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="c-board-wrapper">
+      {jobs.map((job, index) => (
+        <JobItem key={index} job={job} />
+      ))}
+    </div>
+  );
 }
 
 export default JobList;
