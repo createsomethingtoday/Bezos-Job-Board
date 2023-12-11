6c0e564e3fa71d1db6ffa408812cd2d4e9aaf78f
Create Something <micah@createsomething.io>
2023-12-10 20:26:03 -0600

    Additional CSS for presentation

--- components/Filter.js
+++ components/Filter.js
@@ -2,74 +2,116 @@ import React, { useState } from 'react';
 
 function Filter({ onKeywordFilterChange, onLocationFilterChange, onRemoveFilter, keywordFilters, locationFilters }) {
 
-  const [keywordInput, setKeywordInput] = useState('');
-  const [locationInput, setLocationInput] = useState('');
+const [keywordInput, setKeywordInput] = useState('');
+const [locationInput, setLocationInput] = useState('');
 
-  const handleKeywordChange = (e) => {
-    setKeywordInput(e.target.value);
-  };
+const handleKeywordChange = (e) => {
+setKeywordInput(e.target.value);
+};
 
-  const handleLocationChange = (e) => {
-    setLocationInput(e.target.value);
-  };
+const handleLocationChange = (e) => {
+setLocationInput(e.target.value);
+};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    if (keywordInput) {
-      onKeywordFilterChange(keywordInput);
-      setKeywordInput(''); 
-    }
+const handleSubmit = (e) => {
+e.preventDefault();
 
-    if (locationInput) {
-      onLocationFilterChange(locationInput);
-      setLocationInput('');
-    }
-  };
+if (keywordInput) {
+onKeywordFilterChange(keywordInput);
+setKeywordInput(''); 
+}
 
-  return (
-    <div className="c-board-search">
-      <form onSubmit={handleSubmit} className="c-search-wrapper">
-        <div className="c-search-input">
-          <input 
-            type="text"
-            value={keywordInput}
-            onChange={handleKeywordChange}
-            placeholder="Search Keyword" 
-          />
-        </div>
+if (locationInput) {
+onLocationFilterChange(locationInput);
+setLocationInput('');
+}
+};
 
-        <div className="c-search-input">
-          <input
-            type="text" 
-            value={locationInput}
-            onChange={handleLocationChange}
-            placeholder="Search Location" 
-          />
-        </div>
+return (
+<div className="c-board-search">
+<form onSubmit={handleSubmit} className="c-search-wrapper">
 
-        <button type="submit">
-          Search
-        </button>
-      </form>
+<input 
+className="c-search-input"
+type="text"
+value={keywordInput}
+onChange={handleKeywordChange}
+placeholder="Search Keyword" 
+/>
 
-      <div className="filter-tags">
-        {keywordFilters?.map((filter, index) => (
-          <div key={index} className="filter-tag">
-            {filter}
-            <button onClick={() => onRemoveFilter(filter)}>X</button>
-          </div>
-        ))}
-        
-        {locationFilters?.map((filter, index) => (
-          <div key={index} className="filter-tag">
-            {filter}
-            <button onClick={() => onRemoveFilter(filter)}>X</button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+
+
+<input
+className="c-search-input"
+type="text" 
+value={locationInput}
+onChange={handleLocationChange}
+placeholder="Search Location" 
+/>
+
+<button type="submit"
+className='c-search-button'>
+Search
+</button>
+</form>
+
+<div className="filter-tags">
+{keywordFilters?.map((filter, index) => (
+<div key={index} className="filter-tag">
+{filter}
+<button onClick={() => onRemoveFilter(filter)}>X</button>
+</div>
+))}
+
+{locationFilters?.map((filter, index) => (
+<div key={index} className="filter-tag">
+{filter}
+<button onClick={() => onRemoveFilter(filter)}>X</button>
+</div>
+))}
+</div>
+
+<style jsx>{`
+.c-board-search {
+background-color: #1c478c;
+padding: 40px;
+}
+
+.c-search-wrapper {
+grid-column-gap: 20px;
+grid-row-gap: 20px;
+display: flex;
+}
+
+.c-search-input {
+height: 20px;
+background-color: #fff;
+border: 1px solid #e7e7e7;
+border-radius: 50px;
+flex: 1;
+padding: 20px;
+}
+
+.c-search-button {
+border-radius: 100%;
+height: 60px;
+width: 60px;
+}
+
+.filter-tags {
+grid-column-gap: 10px;
+grid-row-gap: 10px;
+color: #fff;
+padding: 10px;
+display: flex;
+}
+
+.filter-tag {
+
+}
+`}</style>
+</div>
+);
 }
 
 export default Filter;
