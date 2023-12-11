974062d757239fffa04ad0f41d08b3e423214998
Create Something <micah@createsomething.io>
2023-12-10 19:24:58 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/(.*?)',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'ALLOW-FROM https://bezosacademstg.wpengine.com/',
+            },
+            {
+              key: 'Content-Security-Policy',
+              value: "frame-ancestors 'self' https://bezosacademstg.wpengine.com/",
+            },
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: 'https://bezosacademstg.wpengine.com/',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
