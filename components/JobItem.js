ff7f19ae8ff618b26a81190be541dfc4dbebe65c
Create Something <micah@createsomething.io>
2023-12-11 14:55:49 -0600

    Update Job Item Component to Display Formatted Date
    This change log shows an update to the JobItem component to add date formatting for the job updated_at property. A new formatDate utility function was added to convert the date string to a more user-friendly format before display. This improves readability of the job item card.

--- components/JobItem.js
+++ components/JobItem.js
@@ -1,14 +1,21 @@
+import React from 'react';
 import styles from '../styles/JobList.module.css';
 
 function JobItem({ job }) {
 
   const departments = job.departments ? job.departments.map(dept => dept.name).join(', ') : 'N/A';
 
+  // Function to format the date
+  const formatDate = (dateStr) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateStr).toLocaleDateString('en-US', options);
+  };
+
   return (
     <div className={styles['c-board-card']}>
       <div className={styles['c-card-content-wrapper']}>
         <div className={styles['c-card-title']}>{job.title}</div>
-        <div className={styles['c-card-label']}>{job.updated_at}</div>
+        <div className={styles['c-card-label']}>{formatDate(job.updated_at)}</div>
       </div>
 
       <div className={styles['c-card-content-wrapper']}>
--- pages/index.js
+++ pages/index.js
@@ -2,78 +2,83 @@ import { useEffect, useState } from 'react';
 
 import JobList from '../components/JobList';
 import Filter from '../components/Filter';
-
 import { fetchJobs } from '../services/greenhouseApi';
 
 export default function Home() {
+  // State
+  const [jobs, setJobs] = useState([]);
+  const [filteredJobs, setFilteredJobs] = useState([]);
+  const [keywordFilters, setKeywordFilters] = useState([]);
+  const [locationFilters, setLocationFilters] = useState([]);
 
-// State
-const [jobs, setJobs] = useState([]);
-const [filteredJobs, setFilteredJobs] = useState([]);
-const [keywordFilters, setKeywordFilters] = useState([]);
-const [locationFilters, setLocationFilters] = useState([]);
-
-// Fetch jobs
-useEffect(() => {
+  // Fetch jobs
+  useEffect(() => {
     fetchJobs(true).then(data => {
-    setJobs(data); 
-    setFilteredJobs(data);
+      setJobs(data); 
+      setFilteredJobs(data);
     });
-}, []);
+  }, []);
 
-// Keyword filter
-const handleKeywordFilter = (newFilter) => {
+  // Function to format the date
+  const formatDate = (dateStr) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateStr).toLocaleDateString('en-US', options);
+  };
+
+  // Keyword filter
+  const handleKeywordFilter = (newFilter) => {
     if (!keywordFilters.includes(newFilter)) {
-    setKeywordFilters(prev => [...prev, newFilter]);
+      setKeywordFilters(prev => [...prev, newFilter]);
     }
-};
+  };
 
-// Location filter 
-const handleLocationFilter = (newFilter) => {
+  // Location filter 
+  const handleLocationFilter = (newFilter) => {
     if (!locationFilters.includes(newFilter)) {
-    setLocationFilters(prev => [...prev, newFilter]);
+      setLocationFilters(prev => [...prev, newFilter]);
     }
-};
+  };
 
-// Remove filter
-const handleRemoveFilter = (filter) => {
+  // Remove filter
+  const handleRemoveFilter = (filter) => {
     setKeywordFilters(prev => prev.filter(f => f !== filter));
     setLocationFilters(prev => prev.filter(f => f !== filter));
-};
+  };
 
-// Filter logic
-useEffect(() => {
+  // Filter logic
+  useEffect(() => {
     const filtered = jobs.filter(job => {
-    const keywordMatch = keywordFilters.every(filter => {
+      const formattedDate = formatDate(job.updated_at);
+      const keywordMatch = keywordFilters.every(filter => {
         return job.title.toLowerCase().includes(filter.toLowerCase()) ||
-        (job.departments && job.departments.some(dept => dept.name.toLowerCase().includes(filter.toLowerCase())));
-    });
+               (job.departments && job.departments.some(dept => dept.name.toLowerCase().includes(filter.toLowerCase()))) ||
+               formattedDate.toLowerCase().includes(filter.toLowerCase());
+      });
 
-    const locationMatch = !locationFilters.length || locationFilters.some(filter => {
+      const locationMatch = !locationFilters.length || locationFilters.some(filter => {
         return job.location.name.toLowerCase().includes(filter.toLowerCase());  
-    });
+      });
 
-    return keywordMatch && locationMatch;
+      return keywordMatch && locationMatch;
     });
 
     setFilteredJobs(filtered);
 
-}, [keywordFilters, locationFilters, jobs]);
+  }, [keywordFilters, locationFilters, jobs]);
 
-return (
+  return (
     <div>
-    <Filter
-  onKeywordFilterChange={handleKeywordFilter}
-  onLocationFilterChange={handleLocationFilter}
-  onRemoveFilter={handleRemoveFilter} // Changed from onRemove to onRemoveFilter
-  keywordFilters={keywordFilters}
-  locationFilters={locationFilters}
-/>
-    <p>
+      <Filter
+        onKeywordFilterChange={handleKeywordFilter}
+        onLocationFilterChange={handleLocationFilter}
+        onRemoveFilter={handleRemoveFilter}
+        keywordFilters={keywordFilters}
+        locationFilters={locationFilters}
+      />
+      <p>
         <span>{filteredJobs.length}</span> of <span>{jobs.length}</span>
-    </p>
-
-    <JobList jobs={filteredJobs} />
+      </p>
+      <JobList jobs={filteredJobs} />
     </div>
-);
+  );
 }
