39540b4d82fbc2f19c0141845ad38dedfb066aff
Create Something <micah@createsomething.io>
2024-01-31 10:57:09 -0600

    Update next.config
    To include https://bezosacademy.org/

--- next.config.js
+++ next.config.js
@@ -1,27 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: '/(.*?)',
-          headers: [
-            {
-              key: 'X-Frame-Options',
-              value: 'ALLOW-FROM https://bezosacademstg.wpengine.com/',
-            },
-            {
-              key: 'Content-Security-Policy',
-              value: "frame-ancestors 'self' https://bezosacademstg.wpengine.com/",
-            },
-            {
-              key: 'Access-Control-Allow-Origin',
-              value: 'https://bezosacademstg.wpengine.com/',
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  async headers() {
+    return [
+      {
+        source: '/(.*?)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://bezosacademstg.wpengine.com/ https://bezosacademy.org/', 
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://bezosacademstg.wpengine.com/ https://bezosacademy.org/",
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://bezosacademstg.wpengine.com/ https://bezosacademy.org/',
+          },
+        ],  
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
