a9c7f347f448c3357e6e8fd7acdd7688500508e6
Create Something <micah@createsomething.io>
2024-04-16 01:18:17 -0500

    Update next.config.js
    Include Webflow to test iframe embed.

--- next.config.js
+++ next.config.js
@@ -7,17 +7,17 @@ const nextConfig = {
         headers: [
           {
             key: 'X-Frame-Options',
-            value: 'ALLOW-FROM https://bezosacademstg.wpengine.com/ https://bezosacademy.org/', 
+            value: 'ALLOW-FROM https://bezosacademstg.wpengine.com/ https://bezosacademy.org/ https://bezos-academy-job-board.webflow.io/',
           },
           {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://bezosacademstg.wpengine.com/ https://bezosacademy.org/",
+            value: "frame-ancestors 'self' https://bezosacademstg.wpengine.com/ https://bezosacademy.org/ https://bezos-academy-job-board.webflow.io/",
           },
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://bezosacademstg.wpengine.com/ https://bezosacademy.org/',
+            value: 'https://bezosacademstg.wpengine.com/ https://bezosacademy.org/ https://bezos-academy-job-board.webflow.io/',
           },
-        ],  
+        ],
       },
     ];
   },
