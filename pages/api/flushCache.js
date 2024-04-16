2841babd7cb6105de603c6c5c1cbd167dc907f52
Create Something <micah@createsomething.io>
2024-04-16 01:10:29 -0500

    Include CORS

--- pages/api/flushCache.js
+++ pages/api/flushCache.js
@@ -1,8 +1,26 @@
 // pages/api/flushCache.js
 import { cache } from '../../services/greenhouseApi';
 import { cache as apiCache } from './greenhouseJobs';
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['POST', 'OPTIONS'],
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
+
   if (req.method === 'POST') {
     try {
       // Clear the cache in greenhouseApi.js
@@ -19,7 +37,8 @@ export default async function handler(req, res) {
       res.status(200).json({ message: 'Cache flushed successfully' });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Internal server error' });    }
+      res.status(500).json({ message: 'Internal server error' });
+    }
   } else {
     res.status(405).json({ message: 'Method not allowed' });
   }
